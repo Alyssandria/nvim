0e23b644f7e0b94f386197fb9eaeefafef84ff56
Alyssandria <sanchez.roniel.work@gmail.com>
2025-02-24 20:04:13 +0800

    split keymaps

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -5,3 +5,5 @@ vim.g.maplocal = " "
 -- COMMANDS
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", {noremap = true, silent = true})
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {silent = true})
+vim.keymap.set("n", "<C-w>v", "vsplit", {silent = true})
+
