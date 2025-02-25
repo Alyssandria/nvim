417fc67501223e22ce5b14c812f4e650169eedb8
Alyssandria <sanchez.roniel.work@gmail.com>
2025-02-25 23:31:30 +0800

    gcc compile keymap

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -4,6 +4,9 @@ vim.g.maplocal = " "
 
 -- COMMANDS
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", {noremap = true, silent = true, desc = "Live grep"})
+vim.keymap.set("n", "<leader>9", ":w<CR>:!gcc -o %:r % && %:r.exe<CR>", {noremap = true, silent = true})
+
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {silent = true})
 vim.keymap.set("n", "<C-w>v", ":vsplit<CR>",{silent = true})
 
